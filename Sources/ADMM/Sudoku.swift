a8ae0815e333db89b6e958006e9c364fb10d9f1d
Nate Derbinsky <nate.derbinsky@gmail.com>
2020-06-07 10:10:33 -0400

    added sudoku as example app

---
+++ Sources/ADMM/Sudoku.swift
@@ -0,0 +1,94 @@
+public class Sudoku {
+    /// Adds variables and constraints for a supplied Sudoku
+    /// puzzle to an existing objective
+    ///
+    /// - Parameters:
+    ///   - objective: existing objective graph
+    ///   - innerSide: the side of each inner square of the puzzle (e.g., 3 is standard)
+    ///   - known: sparse representation of each *given* value; indexes are 0..<(innerSide^4), so 0-80 for standard; values are 0..<(innerSide^2), so 0-8 for standard
+    ///
+    /// - returns: binary variables for each location/value possibility; outer array is organized per location 0..<(innerSide^4), inner array is per possible value, 0..<(innerSide^2)
+    public static func addToObjective(objective obj: ObjectiveGraph, innerSide: Int, known: [Int:Int]) -> ContiguousArray<ContiguousArray<VariableNode>> {
+        let outerSide = innerSide * innerSide
+        var vars = ContiguousArray<ContiguousArray<VariableNode>>()
+        
+        for i in 0..<(outerSide * outerSide) {
+            let knownValue: Int
+            let knownWeight: MessageWeight
+            
+            if let knownResult = known[i] {
+                knownValue = knownResult
+                knownWeight = .inf
+            } else {
+                knownValue = -1
+                knownWeight = .std
+            }
+            
+            let options = ContiguousArray<VariableNode>(outerSide.upToExcluding().map { obj.createVariable(initialValue: $0 == knownValue ? 1.0 : 0.0, initialWeight: knownWeight) })
+            
+            vars.append(options)
+            
+            let _ = createOneHotFactor(objective: obj, vars: options)
+            if knownValue != -1 {
+                let _ = createKnownValueFactor(objective: obj, variable: options[knownValue], value: 1.0)
+            }
+        }
+        
+        let rowColSquareRange = outerSide.upToExcluding()
+        
+        for i in rowColSquareRange {
+            for val in rowColSquareRange {
+                // i is row, $0 is col
+                let rowValVars: [VariableNode] = rowColSquareRange.map { vars[ i*outerSide + $0 ][val] }
+                
+                // i is col, $0 is row
+                let colValVars: [VariableNode] = rowColSquareRange.map { vars[ $0*outerSide + i ][val] }
+                
+                // i is square, $0 is within-square index
+                let squareValVars: [VariableNode] = rowColSquareRange.map {
+                    let squareRow = i / innerSide
+                    let squareCol = i % innerSide
+                    
+                    let row = $0 / innerSide
+                    let col = $0 % innerSide
+                    
+                    let varIndex = (((squareRow * innerSide) + row) * outerSide) + ((squareCol * innerSide) + col)
+                    
+                    return vars[varIndex][val]
+                }
+                
+                let _ = createOneHotFactor(objective: obj, vars: rowValVars)
+                let _ = createOneHotFactor(objective: obj, vars: colValVars)
+                let _ = createOneHotFactor(objective: obj, vars: squareValVars)
+            }
+        }
+        
+        return vars
+    }
+    
+    /// Extracts the state of solving a Sudoku puzzle, represented via
+    /// the binary variables of the objective graph
+    ///
+    /// - Parameter objective: objective graph
+    /// - Parameter variables: binary variables associated with the sudoku problem
+    ///
+    /// - returns: value at each cell of the Sudoku grid (0..<(innerSide^2))
+    public static func extractState(objective obj: ObjectiveGraph, variables: ContiguousArray<ContiguousArray<VariableNode>>) -> [Int] {
+        
+        variables.withUnsafeBufferPointer { vBuffer in
+            return vBuffer.map {
+                var result = -1
+                
+                $0.withUnsafeBufferPointer { innerBuffer in
+                    for (j, v) in innerBuffer.enumerated() {
+                        if obj.getValueUnsafe(v) > 0.99 {
+                            result = j
+                        }
+                    }
+                }
+                
+                return result
+            }
+        }
+    }
+}
--- Tests/ADMMTests/ADMMTests.swift
+++ Tests/ADMMTests/ADMMTests.swift
@@ -1,13 +1,135 @@
 import XCTest
 
-@testable import ADMM
+import ADMM
 
 final class ADMMTests: XCTestCase {
-    func testWeightsADMM() {
-//        XCTAssertEqual(foo, bar)
+    private func _sudokuPerformance(algorithm: Algorithm, concurrent: Bool) -> (ObjectiveGraph, ContiguousArray<ContiguousArray<VariableNode>>, [Int]) {
+        let obj = ObjectiveGraph(algorithm: algorithm, learningRate: 1.0, concurrent: concurrent)
+        
+        let given = [62: 5, 130: 6, 91: 8, 2: 4, 114: 0, 222: 2, 139: 7, 39: 5, 102: 2, 60: 0,
+                     188: 10, 148: 2, 42: 15, 224: 13, 28: 1, 119: 6, 85: 14, 135: 4, 170: 8,
+                     158: 7, 146: 5, 58: 7, 181: 0, 36: 7, 180: 15, 33: 10, 107: 9, 253: 10,
+                     125: 2, 141: 8, 252: 6, 178: 9, 97: 4, 216: 6, 74: 12, 128: 0, 213: 8,
+                     152: 9, 75: 15, 183: 8, 199: 2, 77: 14, 240: 9, 116: 4, 113: 1, 194: 1,
+                     173: 0, 31: 13, 248: 12, 112: 12, 164: 12, 101: 15, 67: 5, 88: 0, 43: 1,
+                     246: 3, 238: 0, 7: 3, 232: 11, 196: 6, 103: 13, 138: 10, 195: 14, 153: 0,
+                     6: 12, 72: 3, 1: 5, 212: 0, 117: 3, 166: 11, 250: 0, 82: 10, 193: 0,
+                     120: 14, 197: 9, 254: 13, 227: 7, 109: 11, 3: 9, 23: 0, 15: 14, 154: 14,
+                     61: 15, 17: 11, 59: 14, 219: 3, 136: 13, 5: 11, 142: 15, 89: 6, 127: 8,
+                     9: 13, 56: 2, 44: 3, 143: 9, 249: 15, 167: 9, 211: 10]
+        
+        let variables = Sudoku.addToObjective(objective: obj, innerSide: 4, known: given)
+        
+        let solution = [15, 5, 4, 9, 1, 11, 12, 3, 10, 13, 6, 0, 2, 7, 8, 14, 8, 11, 7, 2, 14, 6, 15,
+        0, 5, 3, 4, 12, 1, 9, 10, 13, 14, 10, 13, 0, 7, 2, 9, 5, 8, 11, 15, 1, 3, 6,
+        4, 12, 1, 3, 12, 6, 13, 4, 8, 10, 2, 9, 7, 14, 0, 15, 5, 11, 2, 9, 8, 5, 11,
+        1, 0, 7, 3, 4, 12, 15, 13, 14, 6, 10, 7, 13, 10, 11, 9, 14, 5, 12, 0, 6, 2,
+        8, 15, 1, 3, 4, 6, 4, 14, 3, 8, 15, 2, 13, 1, 10, 5, 9, 7, 11, 12, 0, 12, 1,
+        0, 15, 4, 3, 10, 6, 14, 7, 11, 13, 5, 2, 9, 8, 0, 2, 6, 12, 3, 5, 14, 4, 13,
+        1, 10, 7, 11, 8, 15, 9, 4, 15, 5, 8, 2, 10, 6, 1, 9, 0, 14, 11, 12, 13, 7, 3,
+        10, 7, 3, 1, 12, 13, 11, 9, 15, 2, 8, 5, 4, 0, 14, 6, 11, 14, 9, 13, 15, 0, 7,
+        8, 4, 12, 3, 6, 10, 5, 1, 2, 3, 0, 1, 14, 6, 9, 4, 2, 7, 5, 13, 10, 8, 12, 11,
+        15, 5, 12, 15, 10, 0, 8, 13, 11, 6, 14, 1, 3, 9, 4, 2, 7, 13, 6, 2, 7, 10, 12,
+        1, 15, 11, 8, 9, 4, 14, 3, 0, 5, 9, 8, 11, 4, 5, 7, 3, 14, 12, 15, 0, 2, 6, 10,
+        13, 1]
+        
+        return (obj, variables, solution)
+    }
+    
+    private static func _reportFindings(_ obj: ObjectiveGraph, _ numIterations: Int, _ description: String) {
+        XCTAssertEqual(0, obj.iterations)
+        XCTAssertFalse(obj.converged)
+        
+        let start = Date()
+        for _ in 1...numIterations {
+            let _ = obj.iterate()
+        }
+        let end = Date()
+        let diff = end.timeIntervalSince(start)
+        
+        let timePerIteration = 1000.0 * diff / Double(numIterations)
+        let timePerIterationEdge = 1000.0 * diff / (Double(numIterations * obj.numEnabledEdges))
+        print("\(description): \(String(format: "%.2f ms/iteration", timePerIteration)), \(String(format: "%.2e ms/iteration-edge", timePerIterationEdge))")
+        
+        XCTAssertFalse(obj.converged)
+        obj.reinitialize()
+    }
+    
+    func testSudoku0a_ADMMSerialPerformance() throws {
+        let (obj, _, _) = _sudokuPerformance(algorithm: .admm, concurrent: false)
+        
+        measure {
+            ADMMTests._reportFindings(obj, 30, "Sudoku (ADMM, Serial)")
+        }
+    }
+    
+    func testSudoku0b_ADMMConcurrentPerformance() throws {
+        let (obj, _, _) = _sudokuPerformance(algorithm: .admm, concurrent: true)
+        
+        measure {
+            ADMMTests._reportFindings(obj, 30, "Sudoku (ADMM, Concurrent)")
+        }
+    }
+    
+    func testSudoku1a_TWASerialPerformance() throws {
+        let (obj, _, _) = _sudokuPerformance(algorithm: .twa, concurrent: false)
+        
+        measure {
+            ADMMTests._reportFindings(obj, 30, "Sudoku (TWA, Serial)")
+        }
+    }
+    
+    func testSudoku1b_TWAConcurrentPerformance() throws {
+        let (obj, _, _) = _sudokuPerformance(algorithm: .twa, concurrent: true)
+        
+        measure {
+            ADMMTests._reportFindings(obj, 30, "Sudoku (TWA, Concurrent)")
+        }
+    }
+    
+    func testSudoku2a_ADMMConverged() throws {
+        let (obj, variables, solution) = _sudokuPerformance(algorithm: .admm, concurrent: true)
+        
+        XCTAssertEqual(0, obj.iterations)
+        XCTAssertFalse(obj.converged)
+        
+        let options = XCTMeasureOptions()
+        options.iterationCount = 3
+        
+        for _ in 1...2 {
+            for _ in 1...2000 {
+                let _ = obj.iterate()
+            }
+            XCTAssertTrue(obj.converged)
+            XCTAssertEqual(Sudoku.extractState(objective: obj, variables: variables), solution)
+            // print(obj.iterations) // 1052
+            obj.reinitialize()
+        }
+    }
+    
+    func testSudoku2b_TWAConverged() throws {
+        let (obj, variables, solution) = _sudokuPerformance(algorithm: .twa, concurrent: true)
+        
+        XCTAssertEqual(0, obj.iterations)
+        XCTAssertFalse(obj.converged)
+        
+        for _ in 1...2 {
+            for _ in 1...2000 {
+                let _ = obj.iterate()
+            }
+            XCTAssertTrue(obj.converged)
+            XCTAssertEqual(Sudoku.extractState(objective: obj, variables: variables), solution)
+            // print(obj.iterations) // 168
+            obj.reinitialize()
+        }
     }
 
     static var allTests = [
-        ("testWeightsADMM", testWeightsADMM),
+        ("testSudokuADMMSerialPerformance", testSudoku0a_ADMMSerialPerformance),
+        ("testSudokuADMMConcurrentPerformance", testSudoku0b_ADMMConcurrentPerformance),
+        ("testSudokuTWASerialPerformance", testSudoku1a_TWASerialPerformance),
+        ("testSudokuTWAConcurrentPerformance", testSudoku1b_TWAConcurrentPerformance),
+        ("testSudokuADMMConverged", testSudoku2a_ADMMConverged),
+        ("testSudokuTWAConverged", testSudoku2b_TWAConverged),
     ]
 }
